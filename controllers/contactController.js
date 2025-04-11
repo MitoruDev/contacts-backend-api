cdf1079d76611d2d91e1835290e0c01d93d269c4
Caner Elmas <elmas@volksversand.de>
2025-04-11 11:27:34 +0200

    implement crud operations

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -18,28 +18,60 @@ const createContact = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("All fields are required");
   }
-  res.status(201).json({ message: "Create contact" });
+  const contact = await Contact.create({
+    name,
+    email,
+    phone,
+  });
+  res.status(201).json({ contact });
 });
 
 //@desc Get a contact
 //@route GET /api/contacts/:id
 //@access Public
 const getContact = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Get contact for ${req.params.id}` });
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+  res
+    .status(200)
+    .json({ message: `Get contact for ${req.params.id}`, contact });
 });
 
 //@desc Update a contact
 //@route PUT /api/contacts/:id
 //@access Public
 const updateContact = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update contact for ${req.params.id}` });
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+  const updatedContact = await Contact.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true, runValidators: true }
+  );
+  res
+    .status(200)
+    .json({ message: `Update contact for ${req.params.id}`, updatedContact });
 });
 
 //@desc Delete a contact
 //@route DELETE /api/contacts/:id
 //@access Public
 const deleteContact = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete contact for ${req.params.id}` });
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+  await Contact.findByIdAndDelete(req.params.id);
+  res
+    .status(200)
+    .json({ message: `Delete contact for ${req.params.id}`, contact });
 });
 
 module.exports = {
